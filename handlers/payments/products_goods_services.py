3dda270ae4efde901909ff18443c5a137ad8c527
PyRusAdmin <zh.vitaliy92@yandex.ru>
2025-04-02 00:15:35 +0300

    Рефакторинг кода. Добавлена покупка TelegramMaster-Search-GPT за крипту.

--- handlers/payments/products_goods_services.py
+++ handlers/payments/products_goods_services.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-password_TelegramMaster = 180.00  # Сумма товара пароль TelegramMaster 2.0
-password_TelegramMaster_Commentator = 180.00  # Сумма товара пароль TelegramMaster_Commentator
+password_TelegramMaster = 200.00  # Сумма товара пароль TelegramMaster 2.0
+password_TelegramMaster_Commentator = 200.00  # Сумма товара пароль TelegramMaster_Commentator
 
-TelegramMaster = 1300.00  # Стоимость товара TelegramMaster 2.0
-TelegramMaster_Commentator = 1000.00  # Сумма товара "TelegramMaster_Commentator"
+TelegramMaster = 1500.00  # Стоимость товара TelegramMaster 2.0
+TelegramMaster_Commentator = 1200.00  # Сумма товара "TelegramMaster_Commentator"
 
 payment_installation = 500.00  # Сумма товара "Установка ПО"
 
-TelegramMaster_Search_GPT = 100.00  # Стоимость товара "TelegramMaster_Search_GPT"
+TelegramMaster_Search_GPT = 1000.00  # Стоимость товара "TelegramMaster_Search_GPT"
